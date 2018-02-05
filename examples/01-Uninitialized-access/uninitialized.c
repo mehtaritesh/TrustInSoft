d6b9583a094503a381c581ab6d46b3b738ddda55
Ritesh Mehta <mehta.ritesh@smr-automotive.com>
2018-02-05 16:57:26 +0530

    Update uninitialized.c
    Ritesh_05Feb2018: changing to see TIS impact on uninitialized variable

--- examples/01-Uninitialized-access/uninitialized.c
+++ examples/01-Uninitialized-access/uninitialized.c
@@ -1,7 +1,8 @@
 int main(void)
 {
   int array[5] = {13, 7, 42, 0, 1};
-  int i, n;
+  int i = 0;
+  int n = 0;
 
   for (i = 0; i < 5; i++) {
     /* The variable 'n' is declared at line 4, but is never
